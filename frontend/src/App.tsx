f6eefc21e7d2ba0cb6f5889cf051537883bbddca
Michael Loff <loffm300334@gmail.com>
2026-02-11 14:00:36 -0500

    feature: added Courses page with project and group listings, and coordinator-specific management options.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import LogoutScreen from './pages/LogoutScreen';
-import Home from './pages/Home';
-import { Toaster } from "@/components/ui/sonner"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import LogoutScreen from "./pages/LogoutScreen";
+import Home from "./pages/Home";
+import Course from "./pages/Courses";
+import { Toaster } from "@/components/ui/sonner";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/logout" element={<LogoutScreen />} />
+        <Route path="/course/:id" element={<Course />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
       <Toaster />
